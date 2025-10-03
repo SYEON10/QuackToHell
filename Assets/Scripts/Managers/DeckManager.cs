b59f8144f743b622a4d15dbb48b14f6ce04a5a26
YujinJeon <yujinjun@ewhain.net>
2025-10-03 22:48:00 +0900

    [FIX] 클라에서 골드 차감 튕겼던 문제
    원인: serverrpc requireownership이 기본으로 true여서, false로 바꿈

--- Assets/Scripts/Managers/DeckManager.cs
+++ Assets/Scripts/Managers/DeckManager.cs
@@ -763,20 +763,24 @@ public class DeckManager : NetworkBehaviour
         
         //로컬클라이언트의 인벤토리를 조회해서, 인벤의 개수가 max인지 확인. max면 구매 못 함. 로그도 찍기.
         CardInventoryPresenter myLocalInventoryPresenter = FindAnyObjectByType<CardInventoryPresenter>();
-        if (myLocalInventoryPresenter.IsInventoryMaximum())
+        if (myLocalInventoryPresenter)
         {
-            Debug.Log($"인벤토리 한도를 초과해서 구매 못 합니다. 인벤토리 한도: {GameConstants.Card.maxCardCount}");
-            PurchaseCardResultClientRpc(false, card, clientId, clientRpcParams);
-            PurchaseCardResultClientRpc(false, card, clientId, clientRpcParams);
-            return;
+            if (myLocalInventoryPresenter.IsInventoryMaximum())
+            {
+                Debug.Log($"인벤토리 한도를 초과해서 구매 못 합니다. 인벤토리 한도: {GameConstants.Card.maxCardCount}");
+                PurchaseResultToCardShopClientRpc(false, clientId);
+                PurchaseCardResultClientRpc(false, card, clientId, clientRpcParams);
+                return;
+            }
         }
+        
         
         // 해당 카드가 존재하는지 확인
         int cardItemIdKey = card.cardItemStatusData.cardItemID;
         if (!IsValidCardItemIdKey(cardItemIdKey))
         {
             Debug.Log($"카드가 존재하지 않습니다.");
-            PurchaseCardResultClientRpc(false, card, clientId, clientRpcParams);
+            PurchaseResultToCardShopClientRpc(false, clientId);
             PurchaseCardResultClientRpc(false, card, clientId, clientRpcParams);
             return;
         }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -108,7 +108,7 @@ public class GameManager : NetworkBehaviour
     /// </summary>
     /// <param name="clientId">골드를 차감할 클라이언트 ID</param>
     /// <param name="amount">차감할 골드 양</param>
-    [ServerRpc]
+    [ServerRpc(RequireOwnership = false)]
     public void DeductPlayerGoldServerRpc(ulong clientId, int amount, ServerRpcParams rpcParams = default)
     {
         ulong requesterClientId = rpcParams.Receive.SenderClientId;
