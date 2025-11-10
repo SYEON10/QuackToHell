1615956fe02c43c9c6cdc63fb9fa4fa445613194
YujinJeon <yujinjun@ewhain.net>
2025-11-10 21:29:32 +0900

    [FIX] 타겟 위치로 드래그하기 미니게임 기획대로 완성
    *이미지 안 씌움
    -플레이어 고스트 애니메이션 변경

--- Assets/Scripts/UI/ChildOfUIBase/Popup/Minigame/DragToTargetGameUI.cs
+++ Assets/Scripts/UI/ChildOfUIBase/Popup/Minigame/DragToTargetGameUI.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Text.RegularExpressions;
 
 public class DragToTargetGameUI : UIPopup
 {
+    [Tooltip("Drop Zone Positions: you have to fit index 0 to 4 like gameObject name: DragItem0, DragItem1, DragItem2, DragItem3, DragItem4")]
+    [SerializeField] private RectTransform[] onDroppedFitPositions;
+    
     private bool isComplete = false;
-    private Vector3 startPosition;
-    private GameObject dragItemGameObject;
+    private Vector3[] _onDroppedFitPositions = new Vector3[5];
+    private GameObject[] dragItemGameObject = new GameObject[5];
+
+    private int matchCount = 0;
+    private const int TOTAL_MATCH_COUNT = 5;
     enum Images
     {
-        DropZone,    // 아이템을 놓을 영역
-        DragItem     // 드래그할 아이템
+        DropZone0,    // 아이템을 놓을 영역
+        DropZone1,
+        DropZone2,
+        DropZone3,
+        DropZone4,
+        DragItem0,     // 드래그할 아이템
+        DragItem1,
+        DragItem2,
+        DragItem3,
+        DragItem4,
     }
 
     
@@ -18,50 +33,101 @@ public class DragToTargetGameUI : UIPopup
     {
         base.Init();
         Bind<Image>(typeof(Images));
-        dragItemGameObject = Get<Image>((int)Images.DragItem).gameObject;
-        BindEvent(dragItemGameObject,OnBeginDrag, GameEvents.UIEvent.BeginDrag);
-        BindEvent(dragItemGameObject,OnEndDrag, GameEvents.UIEvent.EndDrag);
-        BindEvent(dragItemGameObject,OnDragging, GameEvents.UIEvent.Drag);
-        
-        GameObject dropZoneGameObject = Get<Image>((int)Images.DropZone).gameObject;
-        BindEvent(dropZoneGameObject, OnDrapped, GameEvents.UIEvent.Drop);
-    }
-    
-    private void OnEndDrag(PointerEventData data)
-    {
-        if (isComplete)
+        for (int i = 0; i < 5; i++)
         {
+            dragItemGameObject[i] = Get<Image>((int)Images.DragItem0 + i).gameObject;
+            BindEvent(dragItemGameObject[i],OnBeginDrag, GameEvents.UIEvent.BeginDrag);
+            BindEvent(dragItemGameObject[i],OnEndDrag, GameEvents.UIEvent.EndDrag);
+            BindEvent(dragItemGameObject[i],OnDragging, GameEvents.UIEvent.Drag);
             
+            _onDroppedFitPositions[i] = dragItemGameObject[i].transform.position;
+        
+            GameObject dropZoneGameObject = Get<Image>((int)Images.DropZone0 + i).gameObject;
+            BindEvent(dropZoneGameObject, OnDropped, GameEvents.UIEvent.Drop);
         }
-        else
-        {
-            dragItemGameObject.transform.position = startPosition;
-        }
-      
+        
     }
 
-    private void OnDrapped(PointerEventData data)
+    int ReturnDragItemGameObjectIndex(string objectName)
     {
-        Debug.Log(data.pointerDrag.name + " 아이템이 드롭되었습니다!");
-        isComplete = true;
-        OnGameComplete();
+        Match match = Regex.Match(objectName, @"\d+");
+        if (match.Success)
+        {
+            //out int index는 if문 안에서만 사용할 수 있는 index라는 새로운 정수 변수를 선언
+            if (int.TryParse(match.Value, out int index))
+            {
+                return index;
+            }
+        }
+        return -1;
+    }
+    private void OnEndDrag(PointerEventData data)
+    {
+        string objectName = data.pointerDrag.gameObject.name;
+        int index = ReturnDragItemGameObjectIndex(objectName);
+        
+        if (index!=-1)
+        {
+            dragItemGameObject[index].transform.position = _onDroppedFitPositions[index];
+        }
+    }
+
+    private void OnDropped(PointerEventData data)
+    {
+        int myIndex= ReturnDragItemGameObjectIndex(data.pointerEnter.name);
+        int otherIndex = ReturnDragItemGameObjectIndex(data.pointerDrag.name);
+
+        if (myIndex != -1 && otherIndex != -1)
+        {
+            if (myIndex == otherIndex)
+            {
+                matchCount++;
+                
+                //투명컬러
+                Color color = new Color(1f, 1f, 1f, 0.5f);
+                data.pointerDrag.gameObject.GetComponent<Image>().color = color;
+                data.pointerEnter.gameObject.GetComponent<Image>().color = color;
+                SetDragItemPosition(index: otherIndex);
+                if (matchCount == TOTAL_MATCH_COUNT)
+                {
+                    isComplete = true;
+                    OnGameComplete();
+                }
+            }
+            
+        }
+    }
+
+    void SetDragItemPosition(int index)
+    {
+        //해당 인덱스로, 매칭위치 덮어씌우기
+        _onDroppedFitPositions[index] = onDroppedFitPositions[index].position;
     }
 
     private void OnBeginDrag(PointerEventData data)
     {
-        startPosition = dragItemGameObject.transform.position;
+        string objectName = data.pointerDrag.gameObject.name;
+        int index = ReturnDragItemGameObjectIndex(objectName);
+        if (index!=-1)
+        {
+            _onDroppedFitPositions[index] = dragItemGameObject[index].transform.position;
+        }
     }
     
     private void OnDragging(PointerEventData data)
     {
-        //위치 움직이기
-        dragItemGameObject.transform.position = data.position;
+        string objectName = data.pointerDrag.gameObject.name;
+        int index = ReturnDragItemGameObjectIndex(objectName);
+        if (index!=-1)
+        {
+            //위치 움직이기
+            dragItemGameObject[index].transform.position = data.position;
+        }
     }
     
     private void OnGameComplete()
     {
         Debug.Log("타겟 위치로 드래그하기 성공!");
-        gameObject.SetActive(false);
     }
 
 }
